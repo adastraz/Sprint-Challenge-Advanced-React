ac9bac969defb0da16c5fede2d8c1a2fc4a26c68
adastraz <tylergosox@gmail.com>
2020-01-17 12:43:40 -0500

    damn i get too hyperfocused during these...

---
+++ client/src/Card.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+const Pretty = styled.div`
+display:flex;
+flex-direction:column;
+align-items:center;
+width: 22%;
+justify-content: space-around;
+padding:1% 1%;
+background: plum;
+margin:2% 0;
+color:powderblue;
+border-radius:20px;
+`
+const Card = props => {
+    return(
+        <Pretty>
+            <p>{props.item.name}</p>
+            <p>{props.item.country}</p>
+        </Pretty>
+    )
+    
+}
+export default Card
